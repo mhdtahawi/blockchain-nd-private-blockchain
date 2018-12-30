02cc80f034dcbe54e56e2e16889e2421703f7c14
MHD <mhd.tahawi@gmail.com>
2018-12-31 00:04:27 +0100

    add api

---
+++ BlockController.js
@@ -0,0 +1,67 @@
+const SHA256 = require('crypto-js/sha256');
+const BlockClass = require('./Block.js');
+const BlockChain = require('./BlockChain');
+
+/**
+ * Controller Definition to encapsulate routes to work with blocks
+ */
+class BlockController {
+
+    /**
+     * Constructor to create a new BlockController, you need to initialize all your endpoints here
+     * @param {*} app 
+     */
+    constructor(app) {
+        this.app = app;
+        this.blocks = new BlockChain.Blockchain();
+        this.initializeMockData();
+        this.getBlockByIndex();
+        this.postNewBlock();
+    }
+
+    /**
+     * Implement a GET Endpoint to retrieve a block by index, url: "/api/block/:index"
+     */
+    getBlockByIndex() {
+        this.app.get("/api/block/:index", (req, res) => {
+            this.blocks.getBlock(req.params.index)
+            .then(block => res.json(block))
+        });
+    }
+
+    /**
+     * Implement a POST Endpoint to add a new Block, url: "/api/block"
+     */
+    postNewBlock() {
+        this.app.post("/api/block", (req, res) => {
+            const data = req.body.body;
+            if (data){
+                this.blocks.addBlock(new BlockClass.Block(data))
+                    .then(addedBlock => res.json(JSON.parse(addedBlock)));
+            } else {
+                res.status(400).send("Can't create a block without data");
+            }
+        });
+    }
+
+    /**
+     * Helper method to initialize a Mock dataset. It adds 10 test blocks to the blocks array.
+     */
+    initializeMockData() {
+        if(this.blocks.length === 0){
+            for (let index = 0; index < 10; index++) {
+                let blockAux = new BlockClass.Block(`Test Data #${index}`);
+                blockAux.height = index;
+                blockAux.hash = SHA256(JSON.stringify(blockAux)).toString();
+                this.blocks.push(blockAux);
+            }
+        }
+    }
+
+}
+
+/**
+ * Exporting the BlockController class
+ * @param {*} app 
+ */
+module.exports = (app) => { return new BlockController(app);}
